9078cf1497a84f09dfe825bd959622b9fd7a92af
George Brust <george4excellence@yahoo.com>
2020-05-26 23:27:56 -0500

    Add files via upload
    The following functions reworked in R Studio are were used to create a special object that stores a matrix and caches its inverse. The first function, makeCacheMatrix creates a special “matrix”
    The CacheSolve function computes the inverse of the special “matrix” returned by makeCacheMatrix preceeding it. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve  retrievesthe inverse from the cache.
    Following we test the function with a matrix, compute the inverse, retrieve the inverse from the cache list, change the call matrix to the inverse, and then compute the inverse on that and return the original function.

---
+++ MakeCacheMatrixCacheSolve,R.R
@@ -0,0 +1,43 @@
+
+##`makeCacheMatrix`: This function creates a special "matrix" object
+## that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}		
+
+##`cacheSolve`: This function below computes the inverse of the special
+##"matrix" returned by `makeCacheMatrix` above. If the inverse has
+##already been calculated (and the matrix has not changed), then
+##`cacheSolve` should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        i <- x$getinverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
+}	
+B <- matrix(c(1,2,3,4),2,2)
+#solve(B) #We pretend that this cant't happen xD
+
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1) #inverse returned after computation
+
+cacheSolve(B1) #inverse returned from cache
+## getting cached data
